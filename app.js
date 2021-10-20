9175b91ff9f7a4995772df5575ca3c09d47470a1
daudi13 <davouma13@gmail.com>
2021-10-20 11:45:24 +0300

    Commit changes

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ dice.classList.add('hidden');
 const scores = new Array(0, 0);
 let theScore = 0
 let activePlayer = 0;
-let highscore = 0
+
 
 
 
@@ -35,25 +35,34 @@ btnRoll.addEventListener('click', function () {
         document.getElementById(`current--${activePlayer}`).innerHTML = theScore;
 
     } else {
-        document.getElementById(`current--${activePlayer}`).innerHTML = 0;
-        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
-        activePlayer = activePlayer === 1 ? 0 : 1;
-        theScore = 0;
-        document.querySelector(`.player--${activePlayer}`).classList.add('player--active');
+        playerSwitch();
     }
 })
 
 //hold button functionality
 
 btnHold.addEventListener('click', function () {
+
+    //1. add current score to active player's score
+
+    scores[activePlayer] += theScore;
+    playerSwitch();
+
+
+    // 2. check if player's score is >=100 finish the game
+
     
-    if (theScore !== 0) {
-        document.getElementById(`current--${activePlayer}`).innerHTML = 0;
-        highscore += theScore;
-        document.getElementById(`score--${activePlayer}`).innerHTML = highscore;
-        player0El.classList.toggle('player--active');
-        activePlayer = activePlayer === 1 ? 0 : 1;
-        player1El.classList.toggle('player--active');
-        theScore = 0;
+    if (document.getElementById(`score--${activePlayer}`).innerHTML >= 100) {
+        document.getElementById(`score--${activePlayer}`).innerHTML = `WINNER!!`;
     }
+
+    //3. if not switch to the next player
 })
+
+function playerSwitch() {
+    document.getElementById(`score--${activePlayer}`).innerHTML = scores[activePlayer];
+    document.querySelector(`.player--${activePlayer}`).classList.toggle('player--active');
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    document.querySelector(`.player--${activePlayer}`).classList.toggle('player--active');
+    theScore = 0;
+}
